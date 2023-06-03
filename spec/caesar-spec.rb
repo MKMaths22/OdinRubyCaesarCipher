280e65e698d9611d56fc3b5954e7dfdba76dae5d
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-03 14:09:44 +0100

    Correct how to call rspec from terminal, write 5 passing tests

--- spec/caesar-spec.rb
+++ spec/caesar-spec.rb
@@ -1,12 +1,26 @@
 # spec/caesar_spec.rb
 require './cipher'
+
 describe Cipher do
   describe "#caesar_cipher" do
     it "shifts string by one character" do
-      expect(caesar_cipher('cat', 1)).to eql('dbr')
+    expect(subject.caesar_cipher('cat', 1)).to eql('dbu')
+    end
+  
+    it "does nothing when shifting by zero" do
+    expect(subject.caesar_cipher('Peter', 0)).to eql('Peter')
     end
 
+    it "ignores puntuation" do
+    expect(subject.caesar_cipher('?- $', 7)).to eql('?- $')
+    end
 
+    it "maintains upper and lower case" do
+    expect(subject.caesar_cipher('HeLLo wORLd', 4)).to eql('LiPPs aSVPh')
+    end
 
+    it "copes with empty string" do
+    expect(subject.caesar_cipher('', 6)).to eql('')
+    end
   end
 end
